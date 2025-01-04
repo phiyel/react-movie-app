d62eb52f47bbf35ee4543ca5bb4f0f680d0941d8
Malco Raphael <phiyel1@gmail.com>
2025-01-04 13:32:19 -0500

    Dev: load img's over https

--- movie-app/src/config.js
+++ movie-app/src/config.js
@@ -9,7 +9,7 @@ const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en
 // For login and voting
 const REQUEST_TOKEN_URL = `${API_URL}authentication/token/new?api_key=${API_KEY}`;
 
-const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280';
 // w92, w154, w185, w342, w500, w780, original
